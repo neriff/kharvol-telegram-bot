b3b9dfa117307c1d3419c23a1f5527d849a3b2af
Aleksandr Kolomoiets <akolomoiets@ecisys.com>
2024-09-06 22:26:00 +0300

    Initialized project

---
+++ src/main/java/com/kharvoll/kharvollbot/KharvollBotApplication.java
@@ -0,0 +1,13 @@
+package com.kharvoll.kharvollbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KharvollBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KharvollBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kharvoll/kharvollbot/KharvollBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kharvoll.kharvollbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KharvollBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
